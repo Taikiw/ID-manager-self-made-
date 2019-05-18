e7148faea48da3c71cfa0475696439390b82c412
Taikiw <42609366+Taikiw@users.noreply.github.com>
2019-05-18 23:26:58 +0900

    Update idmgrtest.c

--- idmgr/idmgrtest.c
+++ idmgr/idmgrtest.c
@@ -4,7 +4,7 @@
 #include<time.h>
 #include<Windows.h>
 #define MAXCNT 100
-
+// #define CK printf("===%d===",__LINE__);
 typedef struct Manager{
   char Name[30];
   char AccountID[100];
@@ -14,48 +14,31 @@ typedef struct Manager{
   struct Manager *nextmgr;
 }Mgr;
 
+int IDMGR(void);
 int Startmenu(void);
 Mgr *LoadingFile(Mgr *);
-int Registration(Mgr *,Mgr *,int);
-int Swinput(Mgr *,int);
+int Registration(Mgr *,Mgr *);
+int Swinput(Mgr *,Mgr *,int);
+int Check(Mgr *,char *);
+Mgr *Checkup(Mgr *,char *);
+int Updater(Mgr *,char *);
 int Fileoutput(Mgr *);
 int DisplayList(Mgr *); //Display Password List
-int OTPMaker(void); //Onetime Password Maker
-int Selectyn(void); //Checking select answer
+int PwG(void); //Password Generator
+int Select(int); //Checking select answer
+int Updateyn(void); //Checking update
 
 int main(void){
-  int select,selru,numor; //numor : number of registration
-  char update[30];
-  Mgr *mgrp; //Manager pointer
-  Mgr *nextstartp;
-  mgrp = (Mgr *)malloc(sizeof(Mgr));
-  select = Startmenu();
-  printf("\n");
-  switch(select){
+  int selectmain,mode=0;
+  printf("Which one do you want to start ?\n");
+  printf("ID MANAGER :0 Password Generator :1 >");
+  selectmain = Select(mode);
+  switch(selectmain){
     case 0:
-      nextstartp = LoadingFile(mgrp);
-      printf("\tPlease enter the number of regstrations >");
-      scanf(" %d",&numor);
-      while(numor <= 0){
-        printf("\tRe : Please enter the number of regstrations >");
-        scanf(" %d",&numor);
-      }
-      Registration(mgrp,nextstartp,numor);
-      printf("\tEND\n");
+      IDMGR(void);
       break;
     case 1:
-      nextstartp = LoadingFile(mgrp);
-      if(mgrp == nextstartp){
-        printf("\tCannot Continue\n");
-        break;
-      }
-      DisplayList(mgrp);
-      break;
-    case 2:
-      OTPMaker();
-      break;
-    case 3:
-      printf("\tEND\n");
+      PwG(void);
       break;
     default:
       return -1;
@@ -63,36 +46,56 @@ int main(void){
   return 0;
 }
 
+int IDMGR(){
+  int selectmenu,mode=1;
+  selectmenu = Startmenu(mode);
+  Mgr *mgrp;
+  Mgr *nextstartp;
+  mgrp = (Mgr *)malloc(sizeof(Mgr));
+  nextstartp = LoadingFile(mgrp);
+  switch(selectmenu){
+    case 0:
+      Regstration(mgrp,nextstartp);
+    case 1:
+      if(mgrp == nextstartp){
+        printf("\tCannot Continue.\n");
+        return -1;
+      }
+      DisplayList(mgrp);
+      break;
+    case 2:
+      printf("END\n");
+    default:
+      return -1;
+  }
+  return 0;
+}
+
 int Startmenu(){
-  int select;
+  int mode=1;
   system("cls");
   printf("\n");
   printf("                ----------------------------------------------------\n");
   printf("                          ----- ID MANAGER START MENU ----          \n");
   printf("                ----------------------------------------------------\n");
-  printf("\tRegistration :0 Display Password List :1 Making Onetime Password :2 END :3 >");
-  scanf(" %d",&select);
-  while(select < 0 || 3 < select){
-		printf("\tRe: Registration :0 Display Password List :1 Making Onetime Password :2 END :3 >");
-		scanf(" %d",&select);
-	}
-  return select;
+  printf("\tRegistration or Change management :0 Display Password List :1 END :2 >");
+  return Select(mode);
 }
+
 Mgr *LoadingFile(Mgr *lmgrp){
-  int selans,load; //sel : Select answer
+  int afsearch,load; //afserch : available flie search
   Mgr *nextlmgrp;
-  Mgr *retmgrp;
+  Mgr *retmgrp; //ret : return
   FILE *lfp; //Loading file pointer
-  printf("\tIs that the first time for you to use this ? Yes : y or No : n >");
-  selans = Selectyn();
-  if(selans == 1){
+  afsearch =
+  if(afsearch == 1){
     if((lfp=fopen("ID_data.txt","w"))==NULL){
       printf("\tCannot Open\n");
       exit(0);
     }
-    printf("\tMaking new file ID_data.txt\n");
+    printf("\tCreated new file ID_data.txt\n");
     fclose(lfp);
-  }else if(selans == 0){
+  }else if(afsearch == 0){
     printf("\tData Loading...\n");
   }
   if((lfp=fopen("ID_data.txt","r"))==NULL){
@@ -106,6 +109,8 @@ Mgr *LoadingFile(Mgr *lmgrp){
     if(fscanf(lfp,"%[^,],%[^,],%[^,],%[^,],%s\n",lmgrp->Name,lmgrp->AccountID,lmgrp->MailAdress1,lmgrp->MailAdress2,lmgrp->PassWord)==EOF){
       free(lmgrp->nextmgr);
       lmgrp->nextmgr = NULL;
+      fclose(lfp);
+      printf("\tFinish Loading\n");
       return lmgrp;
     }
     lmgrp = lmgrp->nextmgr;
@@ -115,7 +120,8 @@ Mgr *LoadingFile(Mgr *lmgrp){
   retmgrp = lmgrp->nextmgr;
   return retmgrp;
 }
-int Registration(Mgr *startmgrp,Mgr *rmgrp,int numor){
+
+int Registration(Mgr *startmgrp,Mgr *rmgrp){
   int reg,inp; //reg : Register
   FILE *rfp; //Register file pointer
   printf("      \tDATA REGISTER\n");
@@ -137,10 +143,11 @@ int Registration(Mgr *startmgrp,Mgr *rmgrp,int numor){
   Fileoutput(startmgrp);
   return 0;
 }
+
 int Swinput(Mgr *swmgrp,int swinp){
   switch(swinp){
     case 0:
-      printf("--------------------------------------------------------------\n");
+      printf("-------------------------------------------------------------\n");
       printf("\tSite Name >");
       scanf(" %s",swmgrp->Name);
       break;
@@ -159,13 +166,18 @@ int Swinput(Mgr *swmgrp,int swinp){
     case 4:
       printf("\tPass Word >");
       scanf(" %s",swmgrp->PassWord);
-      printf("--------------------------------------------------------------\n");
+      printf("-------------------------------------------------------------\n");
       break;
     default:
       return -1;
   }
   return 0;
 }
+
+int Check(){
+
+}
+
 int Fileoutput(Mgr *fomgrp){
   Mgr *nextfomgrp;
   FILE *cpyfp;
@@ -191,42 +203,58 @@ int Fileoutput(Mgr *fomgrp){
   fclose(cpyfp);
   system("copy /B newdata.txt ID_data.txt");
   if(DeleteFile("newdata.txt")){
-    printf("\tStorage Complete\n");
+    printf("\tStorage Complete.\n");
   }else{
-    printf("\tCannot File Delete\n");
+    printf("\tCannot File Delete.\n");
     exit(0);
   }
   return 0;
 }
-int DisplayList(Mgr *dmgrp){
-  Mgr *next;
-  while(dmgrp->nextmgr != NULL){
-    printf("--------------------------------------------------------------\n");
-    printf("\tSite Name : %s\n",dmgrp->Name);
-    printf("\tAccount ID : %s\n",dmgrp->AccountID);
-    printf("\tMail Adress1 : %s\n",dmgrp->MailAdress1);
-    printf("\tMail Adress2 : %s\n",dmgrp->MailAdress2);
-    printf("\tPass Word : %s\n",dmgrp->PassWord);
-    if(dmgrp->nextmgr != NULL){
-      next = dmgrp->nextmgr;
-      free(dmgrp);
-      dmgrp = next;
-    }
-  }
-  free(dmgrp);
-  printf("--------------------------------------------------------------\n");
-  printf("\t* Finish *\n");
-  return 0;
+
+int Select(int mode){
+  int select;
+  char selectyn;
+  switch(mode){
+    case 0:
+      scanf("%d",&selct);
+      while(select < 0 || 1 < select){
+        printf("\tRe: ID MANAGER :0 Password Generator :1 >");
+        scanf(" %d",&select);
+      }
+      break;
+    case 1:
+      scanf(" %d",&select);
+      while(select < 0 || 2 < select){
+        printf("\tRe: Registration or Change management:0 Display Password List :1 END :2 >");
+        scanf(" %d",&select);
+      }
+      break;
+    case 2:
+      scanf(" %c",&selectyn);
+      while((selectyn != 'y') && (selectyn != 'n')){
+        printf("\tRe: Yes : y or No : n >");
+        scanf(" %c",&selectyn);
+      }
+      if(selectyn =='y'){
+        return 1;
+      }
+      if(selectyn =='n'){
+        return 0;
+      }
+      return 1;
+    default:
+      return -1;
 }
-int OTPMaker(){
-	int oynans,numoc,c,r; //numoc : Number of characters
+
+int PwG(void){
+	int ynans,numoc,c,r,mode=2; //numoc : Number of characters
 	char *pw;
 	printf("\tIncluding upper case letter ? Yes : y or No : n >");
-  oynans = Selectyn();
+  ynans = Select(mode);
 	printf("\tPlease enter the number of characters >");
 	scanf("%d",&numoc);
 	pw = (char *) malloc (sizeof(char) * (numoc+1));
-	if(oynans == 1){
+	if(ynans == 1){
 		srand((unsigned)time(NULL));
 		for(c=0;c<numoc;c++){
 			r = rand()%3;
@@ -242,7 +270,7 @@ int OTPMaker(){
 					break;
 				}
 		}
-	} else if(oynans == 0){
+	} else if(ynans == 0){
 		srand((unsigned)time(NULL));
 		for(c=0;c<numoc;c++){
 			r = rand()%2;
@@ -257,22 +285,7 @@ int OTPMaker(){
 		}
 	}
 	*(pw+numoc) = '\0';
-	printf("\tOnetime Password : %s\n",pw);
+	printf("\tPassword : %s\n",pw);
   free(pw);
 	return 0;
-}
-int Selectyn(){
-  char selectyn;
-  scanf(" %c",&selectyn);
-  while((selectyn != 'y') && (selectyn != 'n')){
-		printf("\tRe: Yes : y or No : n >");
-    scanf(" %c",&selectyn);
-  }
-  if(selectyn =='y'){
-    return 1;
-  }
-  if(selectyn =='n'){
-    return 0;
-  }
-  return 1;
 }
