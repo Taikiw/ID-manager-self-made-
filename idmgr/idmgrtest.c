e2c909d617289b5d5c550a32da0bc9e3f07fc1b8
Taikiw <42609366+Taikiw@users.noreply.github.com>
2019-05-19 00:49:31 +0900

    Update idmgrtest.c

--- idmgr/idmgrtest.c
+++ idmgr/idmgrtest.c
@@ -18,10 +18,8 @@ int IDMGR(void);
 int Startmenu(void);
 Mgr *LoadingFile(Mgr *);
 int Registration(Mgr *,Mgr *);
+int Administration(Mgr *);
 int Swinput(Mgr *,Mgr *,int);
-int Check(Mgr *,char *);
-Mgr *Checkup(Mgr *,char *);
-int Updater(Mgr *,char *);
 int Fileoutput(Mgr *);
 int DisplayList(Mgr *); //Display Password List
 int PwG(void); //Password Generator
@@ -31,7 +29,8 @@ int Updateyn(void); //Checking update
 int main(void){
   int selectmain,mode=0;
   printf("Which one do you want to start ?\n");
-  printf("ID MANAGER :0 Password Generator :1 >");
+  printf("Please Enter your select number.");
+  printf("ID MANAGER :'0' Password Generator :'1' >");
   selectmain = Select(mode);
   switch(selectmain){
     case 0:
@@ -49,22 +48,27 @@ int main(void){
 int IDMGR(){
   int selectmenu,mode=1;
   selectmenu = Startmenu(mode);
-  Mgr *mgrp;
-  Mgr *nextstartp;
-  mgrp = (Mgr *)malloc(sizeof(Mgr));
-  nextstartp = LoadingFile(mgrp);
+  Mgr *hmgrp;
+  Mgr *nextmgrp;
+  hmgrp = (Mgr *)malloc(sizeof(Mgr));
+  nextmgrp = LoadingFile(hmgrp);
   switch(selectmenu){
     case 0:
-      Regstration(mgrp,nextstartp);
+      Regstration(hmgrp,nextmgrp);
+      break;
     case 1:
-      if(mgrp == nextstartp){
+      Administration(hmgrp);
+      break;
+    case 2:
+      if(hmgrp == nextmgrp){
         printf("\tCannot Continue.\n");
         return -1;
       }
-      DisplayList(mgrp);
+      DisplayList(hmgrp);
       break;
-    case 2:
-      printf("END\n");
+    case 3:
+      printf("END.\n");
+      break;
     default:
       return -1;
   }
@@ -78,7 +82,7 @@ int Startmenu(){
   printf("                ----------------------------------------------------\n");
   printf("                          ----- ID MANAGER START MENU ----          \n");
   printf("                ----------------------------------------------------\n");
-  printf("\tRegistration or Change management :0 Display Password List :1 END :2 >");
+  printf("\tRegistration :'0' Change management :'1' Display Password List :'2' END :'3' >");
   return Select(mode);
 }
 
@@ -93,7 +97,7 @@ Mgr *LoadingFile(Mgr *lmgrp){
       printf("\tCannot Open\n");
       exit(0);
     }
-    printf("\tCreated new file ID_data.txt\n");
+    printf("\tCreated New File ID_data.txt.\n");
     fclose(lfp);
   }else if(afsearch == 0){
     printf("\tData Loading...\n");
@@ -121,10 +125,11 @@ Mgr *LoadingFile(Mgr *lmgrp){
   return retmgrp;
 }
 
-int Registration(Mgr *startmgrp,Mgr *rmgrp){
-  int reg,inp; //reg : Register
-  FILE *rfp; //Register file pointer
+int Registration(Mgr *hmgrp,Mgr *rmgrp){
+  int reg,numor,inp,mode=2; //numor : Number of registrtions
   printf("      \tDATA REGISTER\n");
+  printf("\tPlease enter the number of regstrations >");
+  reg = Select(mode);
   printf("     \t** ATTENTION **\n");
   printf("\tIf there is nothing to enter , please enter 'no'\n");
   for(reg=0;reg<numor;reg++){
@@ -133,14 +138,22 @@ int Registration(Mgr *startmgrp,Mgr *rmgrp){
       printf("\n");
     }
     if(reg == numor-1){
-      rmgrp->nextmgr = NULL;
+      rextmgrp->nextmgr = NULL;
       break;
     }
     rmgrp->nextmgr = (Mgr *)malloc(sizeof(Mgr));
     rmgrp = rmgrp->nextmgr;
   }
   printf("\tRegistration Complete\n");
-  Fileoutput(startmgrp);
+  Fileoutput(hmgrp);
+  return 0;
+}
+
+int Administration(Mgr *hmgrp){
+  int selectadmin,mode=3;
+  printf("      \t*** Setting of ID MANAGER ***\n");
+  pritnf("\tContent Change :'0' Content Delete '1' > ");
+  selectadmin = Select(mode);
   return 0;
 }
 
@@ -172,10 +185,6 @@ int Swinput(Mgr *swmgrp,int swinp){
       return -1;
   }
   return 0;
-}
-
-int Check(){
-
 }
 
 int Fileoutput(Mgr *fomgrp){
@@ -211,50 +220,16 @@ int Fileoutput(Mgr *fomgrp){
   return 0;
 }
 
-int Select(int mode){
-  int select;
-  char selectyn;
-  switch(mode){
-    case 0:
-      scanf("%d",&selct);
-      while(select < 0 || 1 < select){
-        printf("\tRe: ID MANAGER :0 Password Generator :1 >");
-        scanf(" %d",&select);
-      }
-      break;
-    case 1:
-      scanf(" %d",&select);
-      while(select < 0 || 2 < select){
-        printf("\tRe: Registration or Change management:0 Display Password List :1 END :2 >");
-        scanf(" %d",&select);
-      }
-      break;
-    case 2:
-      scanf(" %c",&selectyn);
-      while((selectyn != 'y') && (selectyn != 'n')){
-        printf("\tRe: Yes : y or No : n >");
-        scanf(" %c",&selectyn);
-      }
-      if(selectyn =='y'){
-        return 1;
-      }
-      if(selectyn =='n'){
-        return 0;
-      }
-      return 1;
-    default:
-      return -1;
-}
-
 int PwG(void){
-	int ynans,numoc,c,r,mode=2; //numoc : Number of characters
+	int selectyn,numoc,c,r,mode=4; //numoc : Number of characters
 	char *pw;
 	printf("\tIncluding upper case letter ? Yes : y or No : n >");
-  ynans = Select(mode);
+  selectyn = Select(mode);
 	printf("\tPlease enter the number of characters >");
-	scanf("%d",&numoc);
+  mode = 5;
+  numoc = Select(mode);
 	pw = (char *) malloc (sizeof(char) * (numoc+1));
-	if(ynans == 1){
+	if(selectyn == 1){
 		srand((unsigned)time(NULL));
 		for(c=0;c<numoc;c++){
 			r = rand()%3;
@@ -270,7 +245,7 @@ int PwG(void){
 					break;
 				}
 		}
-	} else if(ynans == 0){
+	} else if(selectyn == 0){
 		srand((unsigned)time(NULL));
 		for(c=0;c<numoc;c++){
 			r = rand()%2;
@@ -288,4 +263,56 @@ int PwG(void){
 	printf("\tPassword : %s\n",pw);
   free(pw);
 	return 0;
+}
+
+int Select(int mode){
+  int select;
+  char selectyn;
+  scanf("%d",&select);
+  switch(mode){
+    case 0:
+      while(select < 0 || 1 < select){
+        printf("\tRe: ID MANAGER :'0' Password Generator :'1' >");
+        scanf(" %d",&select);
+      }
+      return select;
+    case 1:
+      while(select < 0 || 3 < select){
+        printf("\tRe: Registration '0' Change management:'1' Display Password List :'2' END :'3' >");
+        scanf(" %d",&select);
+      }
+      return select;
+    case 2:
+      while(select <= 0){
+        printf("\tRe : Please enter the number of regstrations >");
+        scanf(" %d",&select);
+      }
+      return select;
+    case 3:
+      while(select < 0 || 1 < select){
+        printf("\tRe : Content Change :'0' Content Delete '1' > ");
+        scanf(" %d",&select);
+      }
+      return select;
+    case 4:
+      scanf(" %c",&selectyn);
+      if(selectyn =='y'){
+        return 1;
+      }else if(selectyn =='n'){
+        return 0;
+      }else if((selectyn != 'y') && (selectyn != 'n')){
+        printf("\tRe: Yes : 'y' or No : 'n' >");
+        return Select(mode);
+      }
+      return 1;
+    case 5:
+      while(select < 0){
+        printf("\tRe : Please enter the number of characters >");
+        scanf(" %d",&select);
+      }
+      return select;
+    default:
+      return -1;
+  }
+  return 0;
 }
